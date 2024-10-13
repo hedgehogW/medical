d6eb1ab59ac8c462345f5bf2cfd8a2925670ef11
1Crucioooo <1148338650@qq.com>
2024-10-13 16:40:27 +0800

    feat: 医生端功能05

--- userManagement/src/main/java/com/example/usermanagement/controller/DoctorController.java
+++ userManagement/src/main/java/com/example/usermanagement/controller/DoctorController.java
@@ -1,4 +1,28 @@
 package com.example.usermanagement.controller;
 
+import com.example.usermanagement.service.PreDiagnosisService;
+import com.example.usermanagement.vo.PreDiagnosisResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(tags="医生端")
+@RestController
+@RequestMapping("/doctor")
 public class DoctorController {
+
+    @Autowired
+    private PreDiagnosisService preDiagnosisService;
+
+    @ApiOperation(value = "医生对应病历查询",tags = "test")
+    @PostMapping("/preDiagnosis/{patientId}")
+    List<PreDiagnosisResponse> getPatientPreDiagnosis(@PathVariable Long patientId){
+        System.out.println("111111");
+        List<PreDiagnosisResponse> list =preDiagnosisService.selectPreDiagnosis(1L,patientId);
+        System.out.println("4444444");
+        return list;
+    }
 }
