1654c74d20d6dd8c849e8fe1a67220d41259c519
1Crucioooo <1148338650@qq.com>
2024-10-13 16:38:09 +0800

    feat: 医生端功能03

--- userManagement/src/main/java/com/example/usermanagement/service/PreDiagnosisService.java
+++ userManagement/src/main/java/com/example/usermanagement/service/PreDiagnosisService.java
@@ -2,9 +2,13 @@ package com.example.usermanagement.service;
 
 import com.example.usermanagement.vi.PreDiagnosisRequest;
 import com.example.usermanagement.vo.PreDiagnosisResponse;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface PreDiagnosisService {
     public PreDiagnosisResponse submitPreDiagnosis(Long patientId, PreDiagnosisRequest preDiagnosisRequest);
+
+    public List<PreDiagnosisResponse> selectPreDiagnosis(Long doctorId,Long patientId);
 }
