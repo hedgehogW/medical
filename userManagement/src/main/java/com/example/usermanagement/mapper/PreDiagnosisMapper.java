10627098c510fcf6c1554662b8ced8b629dd958f
1Crucioooo <1148338650@qq.com>
2024-10-13 16:37:02 +0800

    feat: 医生端功能02

--- userManagement/src/main/java/com/example/usermanagement/mapper/PreDiagnosisMapper.java
+++ userManagement/src/main/java/com/example/usermanagement/mapper/PreDiagnosisMapper.java
@@ -3,8 +3,13 @@ package com.example.usermanagement.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.usermanagement.model.PreDiagnosis;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface PreDiagnosisMapper extends BaseMapper<PreDiagnosis> {
-
+    @Select("SELECT * FROM pre_diagnosis WHERE doctor_id = #{doctorId} AND patient_id = #{patientId}")
+    List<PreDiagnosis> findAllByDoctorIdAndPatientId(@Param("doctorId") Long doctorId, @Param("patientId") Long patientId);
 }
